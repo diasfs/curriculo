ec063ffc2a7b8ed7208db49de76666fd8f5c2651
Fábio Dias <fabio@MacBook-de-Fabio-Dias.local>
2019-07-13 21:32:58 -0300

    First Commit

---
+++ App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="app">
+        <div class="header">
+            <h1>Fábio Dias</h1>
+            <h2>Full Stack Developer</h2>
+            <img src="profile.jpg" class="profile"/>
+        </div>
+        <div class="contact columns-2">
+            <div class="text-right">fabio@octopuz.com.br</div>
+            <div>+55 51 98466.3791</div>
+        </div>
+        <div class="columns-2">
+            <div class="text-right"><strong>Born</strong>: July 6, 1985 (age 34)</div>
+            <div>
+                <i class="fas fa-map-marker-alt"></i> Viamão, Rio Grande do Sul, Brazil.
+            </div>
+        </div>
+        <div class="columns-2">
+            <div class="text-right"><h3>Education</h3></div>
+            <div>
+                Escola Aberta do Brasil<br />
+                Informartion Systems
+            </div>
+        </div>
+        <div class="columns-2">
+            <div class="text-right"><h3>Social</h3></div>
+            <div>
+                <i class="fab fa-linkedin"></i> /in/fabio-dias<br />
+                <i class="fab fa-github"></i> /diasfs
+            </div>
+        </div>
+    </div>
+</template>
---
+++ index.js
@@ -0,0 +1,11 @@
+import "./style.less";
+
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import Vue from 'vue';
+import App from './App.vue';
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+});
