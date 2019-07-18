d25a9199f4bef627d485c4991b8be4b1104d1b23
Fábio Dias <binho85@gmail.com>
2019-07-17 23:20:55 -0300

    2019

--- App.vue
+++ App.vue
@@ -1,32 +1,86 @@
 <template>
     <div id="app">
         <div class="header">
+            <br />
+            <br />
+            <br />
             <h1>Fábio Dias</h1>
             <h2>Full Stack Developer</h2>
-            <img src="profile.jpg" class="profile"/>
+            <br />
+            <br />
+            <br />
+            <img src="profile.jpg" class="profile" />
         </div>
         <div class="contact columns-2">
             <div class="text-right">fabio@octopuz.com.br</div>
             <div>+55 51 98466.3791</div>
         </div>
+
         <div class="columns-2">
-            <div class="text-right"><strong>Born</strong>: July 6, 1985 (age 34)</div>
+            <div class="text-right">
+                <strong>Born</strong>: July 6, 1985 (age 34)
+            </div>
             <div>
-                <i class="fas fa-map-marker-alt"></i> Viamão, Rio Grande do Sul, Brazil.
+                <i class="fas fa-map-marker-alt"></i> Viamão / RS, Brazil.
             </div>
         </div>
         <div class="columns-2">
-            <div class="text-right"><h3>Education</h3></div>
+            <div class="text-right">
+                <h3>Education</h3>
+            </div>
             <div>
-                Escola Aberta do Brasil<br />
-                Informartion Systems
+                Escola Superior Aberta do Brasil
+                <br />Informartion Systems
             </div>
         </div>
         <div class="columns-2">
-            <div class="text-right"><h3>Social</h3></div>
+            <div class="text-right">
+                <h3>Social</h3>
+            </div>
             <div>
-                <i class="fab fa-linkedin"></i> /in/fabio-dias<br />
+                <i class="fab fa-linkedin"></i> /in/fabio-dias
+                <br />
                 <i class="fab fa-github"></i> /diasfs
+            </div>
+        </div>
+        <div class="columns-2">
+            <div class="text-right">
+                <h3>About</h3>
+            </div>
+            <div>
+                I work as a full stack web developer since 2009, having developed countless websites, systems and applications in this period.
+                <br />
+                <br />I developed my own CMS, which has been used and improved since 2012. Today I have been looking for time to rewrite it in a more modern way.
+            </div>
+        </div>
+        <div class="columns-2">
+            <div class="text-right">
+                <h3>Expertise</h3>
+            </div>
+            <div>
+                PHP, NodeJs, Javascript, Vue, Nuxt jQuery, Express.js, Mysql, MongoDb, Firebase,
+                GraphQL, HTML 5, Twig, Nunjucks, Backbone.js, Less, CSS, Cordova, Webpack, Parcel,
+                Rollup, Git, Linux, Bash, Apache.
+            </div>
+        </div>
+        <div class="columns-2">
+            <div class="text-right">
+                <h3>Experience</h3>
+            </div>
+            <div>
+                <strong>Full Stack Web Developer</strong>
+                <br />Studio GT, Jun 2009 - Until today
+                <br />
+                <br />Development of websites, e-commerce, systems and mobile apps.
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
             </div>
         </div>
     </div>
